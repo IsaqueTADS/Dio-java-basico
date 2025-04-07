9d167da227f43c8a7da99011ed44d1e780bec1f3
IsaqueTADS <isaqueofc18@gmail.com>
2025-04-07 11:31:59 -0300

    tipos de variaveis em java

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,34 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+
         System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // Se começar com zero, use String (ex: "021070333")
+        long cpf = 98765432109L; // "L" indica que é um literal long (se começar com zero, use String)
+        float pi = 3.14F; // "F" indica que é um literal float
+        double salario = 1275.33;
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.err.println(VALOR_DE_PI);
+
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cep);
+        System.out.println(cpf);
+        System.out.println(pi);
+        System.out.println(salario);
+
     }
 }
+
+/*
+ * int
+ * byte
+ * short
+ * long
+ * float
+ * double
+ * char
+ */
