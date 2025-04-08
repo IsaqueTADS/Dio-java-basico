cd9f4924c69af4960b87ed3527b83fe9d3058a46
IsaqueTADS <isaqueofc18@gmail.com>
2025-04-08 15:02:55 -0300

    refatorandoi o codigo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -19,10 +19,23 @@ public class ContaTerminal {
     System.out.println("Digite o seu nome: ");
     nomeCliente = scanner.nextLine();
     scanner.nextLine();
-    System.out.println("Digite o numero para a conta: ");
+    System.out.println("Digite o saldo da conta: ");
     saldo = scanner.nextDouble();
 
     // Exibindo a mensagem com os dados da conta
+
+    /**
+     * <h1>Poderia ser</h1>
+     * System.out.println("\nOlá ".concat(nomeCliente)
+     * .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+     * .concat(agencia)
+     * .concat(", conta ")
+     * .concat(String.valueOf(numero))
+     * .concat(" e seu saldo ")
+     * .concat(String.valueOf(saldo))
+     * .concat(" já está disponível para saque."));
+     * 
+     */
     System.out.println("\nOlá " + nomeCliente +
         ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
         ", conta " + numero +
